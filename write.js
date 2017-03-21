2033693f056c716f07ee416959ad03b565045685
Alice BC <alice.binghamcarr@gmail.com>
2017-03-21 12:31:38 +0000

    Add files via upload

---
+++ write.js
@@ -0,0 +1,9 @@
+// 2.b
+var fs = require('fs');
+var data = 'Simply Easy Learning';
+var writeStream = fs.createWriteStream('output.txt');
+writeStream.write(data, 'UTF8');
+writeStream.end();
+writeStream.on('finish', function(){
+  console.log('Write completed.');
+});
